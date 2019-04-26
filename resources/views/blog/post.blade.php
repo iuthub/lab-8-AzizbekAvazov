f726a76cc70f5274fd059d60543b95507b8d19a8
Azizbek Avazov <azizbek97_97@list.ru>
2019-04-26 13:13:15 +0500

    lab 8 finished

---
+++ resources/views/blog/post.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <p class="quote">Learning Laravel</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis molestias reprehenderit repellat placeat, fuga distinctio illo odio aliquid consequatur possimus harum commodi praesentium sequi quae omnis veritatis cum nemo iste.!</p>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,46 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+/*Route::get('/', function () {
+    return view('blog.index');
 });
+
+Route::get('/blog/post/{id}', function ($id) {
+    return view('blog.post', ['id'=>$id]);
+});*/
+
+Route::get('/', [
+    'uses' => 'BlogController@getIndex',
+    'as' => 'blog.index'
+]);
+Route::get('post/{id}', [
+    'uses' => 'BlogController@getPost',
+    'as' => 'blog.post'
+]);
+Route::get('about', function () {
+    return view('other.about');
+})->name('other.about');
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('', [
+        'uses' => 'BlogController@getAdminIndex',
+        'as' => 'admin.index'
+    ]);
+    Route::get('create', [
+        'uses' => 'BlogController@getAdminCreate',
+        'as' => 'admin.create'
+    ]);
+    Route::post('create', [
+        'uses' => 'BlogController@postAdminCreate',
+        'as' => 'admin.create'
+    ]);
+    Route::get('edit/{id}', [
+        'uses' => 'BlogController@getAdminEdit',
+        'as' => 'admin.edit'
+    ]);
+    Route::post('edit', [
+        'uses' => 'BlogController@postAdminUpdate',
+        'as' => 'admin.update'
+    ]);
+});
+
